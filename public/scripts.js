aca0c47064934c539148f3a49696dc85993d522c
AzizbekStudent <00013836wiut@gmail.com>
2023-03-27 10:29:41 +0500

    Validations consistent

--- public/scripts.js
+++ public/scripts.js
@@ -12,3 +12,15 @@ function FormValidation_M() {
 }
 // I use this function only for validation of creating new medicine or updating the existing medicine records
 // otherwise application shows error
+
+function FormValidation_C() {
+    const name = document.getElementById("name_c").value.trim();
+    const description = document.getElementById("decs_c").value.trim();
+
+    if (name === "" || description === "") {
+        alert("Please enter a value for Name and Description");
+        return false;
+    }
+
+    return true;
+}
